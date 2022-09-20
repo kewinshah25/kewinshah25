1c0fa5aa36f6d6ddd04b8a93f3a4454dfb00b5a5
Kewin <kewinshah25@gmail.com>
2022-09-19 21:20:00 -0500

    build?

---
+++ docs/assets/data/projects.js
@@ -0,0 +1,32 @@
+[
+  {
+    title: "RoomEase App",
+    techStack: "",
+    description: "...",
+    url: "",
+  },
+  {
+    title: "Code school",
+    techStack: "",
+    description: "...",
+    url: "",
+  },
+  {
+    title: "Restaurant Recommender System",
+    techStack: "",
+    description: "...",
+    url: "",
+  },
+  {
+    title: "Online Video Games Store",
+    techStack: "",
+    description: "...",
+    url: "",
+  },
+  {
+    title: "URL Shortner",
+    techStack: "",
+    description: "...",
+    url: "",
+  },
+];
